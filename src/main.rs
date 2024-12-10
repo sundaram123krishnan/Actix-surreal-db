d57042db79388e20f67f478f49ed0a62b43884a4
sundaram123krishnan <krishnsundaram@gmail.com>
2024-12-10 20:08:22 +0530

    hello actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/greet")]
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello actix")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 5000;
+    HttpServer::new(||
+        App::new().service(greet)
+    ).bind(("127.0.0.1", port)).unwrap().run().await
 }
