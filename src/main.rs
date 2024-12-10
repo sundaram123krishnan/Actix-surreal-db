8a86afa606189c6a3d1a51bad2d9aedc96f61714
sundaram123krishnan <krishnsundaram@gmail.com>
2024-12-10 20:31:01 +0530

    post, patch

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder, post, patch};
 
 #[get("/greet")]
 async fn greet() -> impl Responder {
     HttpResponse::Ok().body("Hello actix")
 }
 
+#[post("/ex_post")]
+async fn ex_post() -> impl Responder {
+    HttpResponse::Ok().body("hello post")
+}
+
+#[patch("/ex_update/{id}")]
+async fn ex_update() -> impl Responder {
+    HttpResponse::Ok().body("hello update")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = 5000;
+    println!("Running on port {}...", port);
     HttpServer::new(||
-        App::new().service(greet)
+        App::new().service(greet).service(ex_post).service(ex_update)
     ).bind(("127.0.0.1", port)).unwrap().run().await
 }
+
+
